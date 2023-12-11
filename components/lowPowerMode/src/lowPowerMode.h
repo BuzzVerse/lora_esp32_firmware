50b0975b35390d373c4f1244970575741994e86d
bykowskiolaf <olafbykowski1@gmail.com>
2023-12-11 20:32:20 +0100

    Renamed to use _ instead of camel case

--- components/lowPowerMode/src/lowPowerMode.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOW_POWER_MODE_H
-#define LOW_POWER_MODE_H
-
-void lowPowerMode_enter_deep_sleep(void);
-
-void lowPowerMode_set_sleep_time(int sleep_time_sec);
-
-#endif // LOW_POWER_MODE_H
---
+++ components/low_power_mode/src/low_power_mode.h
@@ -0,0 +1,8 @@
+#ifndef LOW_POWER_MODE_H
+#define LOW_POWER_MODE_H
+
+void low_power_mode_enter_deep_sleep(void);
+
+void low_power_mode_set_sleep_time(int sleep_time_sec);
+
+#endif // LOW_POWER_MODE_H
