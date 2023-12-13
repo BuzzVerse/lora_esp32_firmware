d95aaeaaf0f677a2493bf22d9b3bff65c3bc2fc2
bykowskiolaf <olafbykowski1@gmail.com>
2023-12-13 22:54:21 +0100

    Initial doxygen config

--- main/main.c
+++ main/main.c
@@ -3,10 +3,17 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+/**
+ * @file main.c
+ * 
+ * @brief This file contains the main function.
+ * 
+ * This file contains the main function. The main function creates a task that enters into the low power mode after 5 seconds.
+ */
 
-void app_main(void){
+void app_main(void)
+{
 
     vTaskDelay(5000 / portTICK_PERIOD_MS);
     xTaskCreate(&low_power_mode_enter_deep_sleep, "deep_sleep_task", 2048, NULL, 5, NULL);
-
 }
