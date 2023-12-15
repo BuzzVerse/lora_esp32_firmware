b0dbc2614d5b164948d3fa02d2e3a64c1da7c058
bykowskiolaf <olafbykowski1@gmail.com>
2023-12-16 00:45:31 +0100

    Changed documentation format and added doxygen-awesome

--- components/low_power_mode/src/low_power_mode.c
+++ components/low_power_mode/src/low_power_mode.c
@@ -3,28 +3,12 @@
 
 const static char *TAG = "low_power_mode_manager";
 
-/**
- * @file low_power_mode.c
- * 
- * @brief This file contains the implementation of the low power mode manager.
-*/
-
-/**
- * This function sets the sleep time for the low power mode. The sleep time is specified in seconds.
- *
- * @param sleep_time_sec The sleep time in seconds.
- */
 void low_power_mode_set_sleep_time(int sleep_time_sec)
 {
     printf("Enabling timer wakeup, %ds\n", sleep_time_sec);
     ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(sleep_time_sec * 1000000));
 }
 
-/**
- * This function makes the device enter into the low power mode. Before entering into the low power mode, make sure to set the sleep time using the function `low_power_mode_set_sleep_time`.
- *
- * @warning All data in the RAM will be lost when the device enters into the low power mode.
- */
 void low_power_mode_enter_deep_sleep(void)
 {
     printf("Entering low power mode\n");
--- main/main.c
+++ main/main.c
@@ -7,13 +7,15 @@
  * @file main.c
  * 
  * @brief This file contains the main function.
- * 
- * This file contains the main function. The main function creates a task that enters into the low power mode after 5 seconds.
  */
 
+/**
+ * @brief This function creates a task that enters into the low power mode after 5 seconds.
+ * 
+ * This function is the entry point of the program. It creates a task that enters into the low power mode after 5 seconds.
+ */
 void app_main(void)
 {
-
     vTaskDelay(5000 / portTICK_PERIOD_MS);
     xTaskCreate(&low_power_mode_enter_deep_sleep, "deep_sleep_task", 2048, NULL, 5, NULL);
 }
