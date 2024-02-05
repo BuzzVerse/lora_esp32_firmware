30c789ddb5a2b986e0e1523ae8a8770708b66956
Rafał Majewski <rafal.majewski92@gmail.com>
2024-02-05 20:42:35 +0100

    added new lora component
    Co-authored-by: Olaf Bykowski <olafbykowski1@gmail.com>

--- main/main.c
+++ main/main.c
@@ -2,55 +2,46 @@
 #include "freertos/task.h"
 #include "esp_err.h"
 #include "esp_log.h"
-
+#include "lora.h"
 #include "bme280.h"
 #include "low_power_mode.h"
 
 #define TAG_BME280 "BME280"
 #define TAG_MAIN "MAIN"
 
+#ifndef CONFIG_RECEIVE_MODE
+
+// Receiver task
 void main_task()
 {
-    esp_err_t esp_rc;
-    double temperature, pressure, humidity;
+}
 
-    esp_rc = bme280_init_driver(CONFIG_BME280_I2C_ADDRESS);
-    esp_rc += bme280_set_oversamp(OVERSAMP_4X, OVERSAMP_4X, OVERSAMP_4X);
-    esp_rc += bme280_set_settings(STANDBY_10MS, FILTER_16, MODE_FORCED);
+#else
 
-    // wait 100ms for the sensor to reach a stable state
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-
-    ESP_LOGI(TAG_BME280, "BME280 init result: %d", esp_rc);
-    if (esp_rc == 0)
+// Transmitter task
+void main_task()
+{
+    for (;;)
     {
-        ESP_LOGI(TAG_BME280, "BME280 init success");
-        for (int i = 1;; i++)
-        {
-            bme280_read_temperature(&temperature);
-            bme280_read_pressure(&pressure);
-            bme280_read_humidity(&humidity);
-
-            ESP_LOGI(TAG_BME280, "Temperature: %.2f C, Pressure: %.2f hPa, Humidity: %.2f %%", temperature, pressure / 100, humidity);
-
-            vTaskDelay(10000 / portTICK_PERIOD_MS);
-
-            low_power_mode_set_sleep_time(10);
-            low_power_mode_enter_deep_sleep();
-        }
-    }
-    else
-    {
-        ESP_LOGE(TAG_BME280, "BME280 init failed. code: %d", esp_rc);
-        esp_restart();
+        vTaskDelay(pdMS_TO_TICKS(5000));
+        lora_send_packet((uint8_t *)"Hello", 5);
+        printf("packet sent...\n");
     }
 }
+
+#endif
 
 // Main application
 void app_main(void)
 {
     TaskHandle_t main_task_handle = NULL;
     int x_task_returned = xTaskCreate(&main_task, "main_task", 2048, NULL, 4, &main_task_handle);
+
+    lora_init();
+    lora_set_frequency(915e6);
+    lora_enable_crc();
+
+    ESP_LOGI(TAG_BME280, "Hello World0");
 
     if (x_task_returned != pdPASS)
     {
